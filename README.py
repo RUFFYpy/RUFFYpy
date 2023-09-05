2ab182769224d651137e80f2230192150007b578
RUFFYpy <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 05:07:29 -0700

    Update README.py

--- README.py
+++ README.py
@@ -1,17 +1,22 @@
 import Profile from "RUFFYpy"
 
 class Bio extends Profile { 
-name = "RUFFY"; 
-email = "nonya@buizzz.net"; 
-website = "https://notexisting.beansoup"; }
+  name = "RUFFY"; 
+  email = "nonya@buizzz.net"; 
+  website = "https://notexisting.beansoup"; 
+  devilFruit = "Gomu Gomu no Mi"; // Luffy's iconic Devil Fruit
+}
 
 class noSkills extends Profile { 
-languages = ["whole lotta nothing"]; 
-frontend = ["licor"]; 
-backend = ["poker"]; desktop = ["too expensive"]; 
-databases = ["yo girls i-messages"]; 
-devOps = ["Got no ops but stay active"]; 
-toolchains = ["glizzy"]; 
-hosting = ["I cook you dinner babe, but don't spill the bean soup"]; 
-operatingSystems = ["cross faded"]; 
-editors = ["the one that works for now"]; }
+  languages = ["whole lotta nothing"]; 
+  frontend = ["licor"]; 
+  backend = ["poker"]; 
+  desktop = ["too expensive"]; 
+  databases = ["yo girls i-messages"]; 
+  devOps = ["Got no ops but stay active"]; 
+  toolchains = ["glizzy"]; 
+  hosting = ["I cook you dinner babe, but don't spill the bean soup"]; 
+  operatingSystems = ["cross faded"]; 
+  editors = ["the one that works for now"]; 
+  crewMembers = ["Nami", "Zoro", "Sanji"]; // Luffy's crewmates
+}
