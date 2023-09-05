b50adc131cf232d4ab9841107416fdb54ababe7f
RUFFYpy <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 01:31:24 -0700

    Create README.py

--- README.py
+++ README.py
@@ -3,15 +3,15 @@ import Profile from "RUFFYpy"
 class Bio extends Profile { 
 name = "RUFFY"; 
 email = "nonya@buizzz.net"; 
-website = "https://nontexisting.beansoup"; }
+website = "https://notexisting.beansoup"; }
 
 class noSkills extends Profile { 
 languages = ["whole lotta nothing"]; 
 frontend = ["licor"]; 
 backend = ["poker"]; desktop = ["too expensive"]; 
-databases = ["yo girl i-messages"]; 
+databases = ["yo girls i-messages"]; 
 devOps = ["Got no ops but stay active"]; 
 toolchains = ["glizzy"]; 
-hosting = ["i cook you dinner babe, but dont spill the bean soup"]; 
+hosting = ["I cook you dinner babe, but don't spill the bean soup"]; 
 operatingSystems = ["cross faded"]; 
-editors = ["the one that works hopefully"]; }
+editors = ["the one that works for now"]; }
