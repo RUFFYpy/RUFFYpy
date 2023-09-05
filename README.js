95bc69673e46ad8b0076d8a1d1a77fa28af84d41
RUFFY.py <143926032+RUFFYpy@users.noreply.github.com>
2023-09-05 01:25:53 -0700

    Create README.js

---
+++ README.js
@@ -0,0 +1,17 @@
+import Profile from "RUFFYpy"
+
+class Bio extends Profile { 
+name = "RUFFY"; 
+email = "nonya@buizzz.net"; 
+website = "https://nontexisting.beansoup"; }
+
+class noSkills extends Profile { 
+languages = ["whole lotta nothing"]; 
+frontend = ["licor"]; 
+backend = ["poker"]; desktop = ["too expensive"]; 
+databases = ["yo girl i-messages"]; 
+devOps = ["Got no ops but stay active"]; 
+toolchains = ["glizzy"]; 
+hosting = ["i cook you dinner babe, but dont spill the bean soup"]; 
+operatingSystems = ["cross faded"]; 
+editors = ["the one that works hopefully"]; }
